de21cfddd75e23a6a085cc404397edb4b65a86c1
FehSchiavon <felipschiavon@gmail.com>
2021-02-03 06:56:33 -0300

    entendendo mais sobre % nos operadores

--- javascript/aulas/javascript-basico/script.js
+++ javascript/aulas/javascript-basico/script.js
@@ -31,7 +31,12 @@ console.log(e)
 // - Subtração
 // * Multiplicação
 // / Divisão
-//  % Módulo
+//  % Módulo é o resto da divisão
 // ++ Incremento
 // -- Decremento
 
+var a = 10;
+var b = 15;
+
+var c = a % b;
+console.log(c)
