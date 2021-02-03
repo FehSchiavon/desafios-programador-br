f4f9e3f2d3cb99e1f42e910967293f48baa1d861
FehSchiavon <felipschiavon@gmail.com>
2021-02-03 06:36:23 -0300

    aprendendo sobre os tipos de dados

--- javascript/aulas/javascript-basico/script.js
+++ javascript/aulas/javascript-basico/script.js
@@ -1,5 +1,14 @@
 // alert("Integrando com arquivo externo")
+// Usando Console
 console.log("Oláá")
 
 var a = 10;
 console.log(a)
+
+// Tipos de Dados
+// Boolean
+// Null
+// Undefined
+// Number
+// String
+
