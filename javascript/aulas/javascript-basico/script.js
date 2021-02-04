47013959ae14fd145bdb8151b873494f61ec731c
FehSchiavon <felipschiavon@gmail.com>
2021-02-04 20:36:30 -0300

    entendo mais sobre operadores logicos

--- javascript/aulas/javascript-basico/script.js
+++ javascript/aulas/javascript-basico/script.js
@@ -76,3 +76,31 @@ a += 5;
 var op = 3;
 var com = 3;
 
+
+
+// ===Operadores Lógicos=== //
+
+// && e
+// || ou
+// Negativo
+
+// var a = false;
+// var b = false;
+
+// var c = a && b
+
+// console.log(c)
+
+// Exemplo com validação de idade
+
+var idade = 25;
+
+var maior20 = idade > 20;
+var menor30 = idade < 30;
+
+var entre = maior20 && menor30
+
+console.log("Idade", idade)
+console.log("Maior que vinte", maior20)
+console.log('Menor que trinta', menor30)
+console.log("Entre 20 e 30", entre);
