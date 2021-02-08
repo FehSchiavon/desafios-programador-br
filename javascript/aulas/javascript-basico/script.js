5b2b3fb1c02f156eff8aff7ade308addc41f170c
FehSchiavon <felipschiavon@gmail.com>
2021-02-08 06:40:34 -0300

    usando e aplicando switch

--- javascript/aulas/javascript-basico/script.js
+++ javascript/aulas/javascript-basico/script.js
@@ -186,20 +186,37 @@ console.log(pode);
 console.log("Switch");
 
 var nota1 = 5.0;
-var note2 = 5.8;
+var nota2 = 5.8;
 
 var media = (nota1 + nota2) / 2;
 
+var conceito = "";
 
+// ==================== //
+if (media >= 8) {
+    conceito = "Ótimo"
+} else if (media >= 6.5) {
+    conceito = "Bom"
+} else {
+    conceito = "Regular"
+}
+
+console.log(media);
+console.log(conceito);
+// ==================== //
 
 switch (conceito) {
     case "Ótimo" :
-    
+        console.log("Parabéns você é um ótimo aluno")
+        break
     case "Bom":
-
+        console.log("Você está quase perfeito")
+        break;
     case "Regular" :
-
+        console.log("Estude mais um pouco");
+        break;
     default:
-
+        console.log("Houve algum erro");
+        break;
 }
 
