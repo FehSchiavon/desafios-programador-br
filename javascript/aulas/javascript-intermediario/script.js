8980ed733ed0fe20a04a145101de23f2a59dddea
FehSchiavon <felipschiavon@gmail.com>
2021-02-18 21:27:26 -0300

    aprendendo a usar o DOM do JS para modificar o HTML

--- javascript/aulas/javascript-intermediario/script.js
+++ javascript/aulas/javascript-intermediario/script.js
@@ -2,4 +2,18 @@
 console.log("=== Aprendo mais sobre o Window ===");
 // Window que dona das propridades [console.log / alert / prompt ...]
 window.console.log("Ele vem do Window")
-alert("É como se fosse POP")
+alert("É como se fosse POP / A propriedade Alert()")
+
+
+
+
+
+
+// Document acessa todo o nosso HTML
+console.log("=== Usado o Document ou DOM");
+window.document
+console.log(document);
+// Acessando o Body
+console.log(document.body);
+// Escrevendo dentro do Body
+console.log(document.body.innerHTML = "<h1>Inserindo um texto por InnerHtml</h1>" + document.body.innerHTML);
