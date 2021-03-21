79dec9de1320ed56d72f7071f8be38c7b862b2ef
FehSchiavon <felipschiavon@gmail.com>
2021-03-21 16:48:35 -0300

    adicionando diferenças para homens/mulhures

--- projects/projeto-churrascometro/scripts.js
+++ projects/projeto-churrascometro/scripts.js
@@ -1,7 +1,15 @@
 // Regra de Negocio
 // Sempre que for criar um programa entenda a regra de negocio
 
-// Carne: 400 Gramas por pessoa + de 6 horas: 650 Gramas
+// Homens
+// Carne Bovina: 400 Gramas por pessoa + de 6 horas: 650 Gramas
+// Carne de Frango: 300 Gramas por pessoa + de 6 horas: 600 Gramas
+// Cerveja: 1200 Millilitros por pessoa + de 6 horas: 2000 Millilitros
+// Refrigerante/ Água: 1000 Millilitros por pessoa + de 6 horas: 1500 Millilitros
+
+// Mulheres
+// Carne Bovina: 400 Gramas por pessoa + de 6 horas: 650 Gramas
+// Carne de Frango: 300 Gramas por pessoa + de 6 horas: 600 Gramas
 // Cerveja: 1200 Millilitros por pessoa + de 6 horas: 2000 Millilitros
 // Refrigerante/ Água: 1000 Millilitros por pessoa + de 6 horas: 1500 Millilitros
 
