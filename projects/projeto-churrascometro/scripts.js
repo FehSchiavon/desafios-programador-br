0a1364b70aa3a6972de07d0f98086148183981c0
FehSchiavon <felipschiavon@gmail.com>
2021-03-21 16:44:15 -0300

    descrevendo a regra de negocio

--- projects/projeto-churrascometro/scripts.js
+++ projects/projeto-churrascometro/scripts.js
@@ -1,2 +1,9 @@
 // Regra de Negocio
 // Sempre que for criar um programa entenda a regra de negocio
+
+// Carne: 400 Gramas por pessoa + de 6 horas: 650 Gramas
+// Cerveja: 1200 Millilitros por pessoa + de 6 horas: 2000 Millilitros
+// Refrigerante/ Água: 1000 Millilitros por pessoa + de 6 horas: 1500 Millilitros
+
+// Crianças comemem metade disso (x0,5)
+
