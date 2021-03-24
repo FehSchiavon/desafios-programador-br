8d83b1cdf679a09b042a21a623f2b4baf7f13745
FehSchiavon <felipschiavon@gmail.com>
2021-03-24 06:37:25 -0300

    selecionando os itens da tela

--- projects/projeto-churrascometro/scripts.js
+++ projects/projeto-churrascometro/scripts.js
@@ -15,4 +15,8 @@
 
 // Crianças comemem metade disso (x0,5)
 
-console.log("JS")
+let inputAdultos = document.getElementById('adultos')
+let inputCriancas = document.getElementById('criancas')
+let inputDuracao = document.getElementById('duracao')
+
+console.log(inputAdultos, inputCriancas, inputDuracao);
