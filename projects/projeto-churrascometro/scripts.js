b2ac73743ff5e0b710fc350f447665d8328c1705
FehSchiavon <felipschiavon@gmail.com>
2021-03-25 06:37:54 -0300

    calculo de cerveja

--- projects/projeto-churrascometro/scripts.js
+++ projects/projeto-churrascometro/scripts.js
@@ -2,9 +2,9 @@
 // Sempre que for criar um programa entenda a regra de negocio
 
 // Homens
-// Carne Bovina: 400 Gramas por pessoa + de 6 horas: 650 Gramas
-// Carne de Frango: 300 Gramas por pessoa + de 6 horas: 600 Gramas
-// Cerveja: 1200 Millilitros por pessoa + de 6 horas: 2000 Millilitros
+// [X] Carne Bovina: 400 Gramas por pessoa + de 6 horas: 650 Gramas 
+// [] Carne de Frango: 300 Gramas por pessoa + de 6 horas: 600 Gramas
+// [X] Cerveja: 1200 Millilitros por pessoa + de 6 horas: 2000 Millilitros
 // Refrigerante/ Água: 1000 Millilitros por pessoa + de 6 horas: 1500 Millilitros
 
 // Mulheres
@@ -28,7 +28,8 @@ function calcular () {
     let criancas = inputCriancas.value
     let duracao = inputDuracao.value
 
-    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) /2 * criancas)
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas)
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos + (cervejaPP(duracao) / 2 * criancas)
     console.log(qdtTotalCarne);
 }
 
@@ -38,5 +39,14 @@ function carnePP (duracao) {
         return 650
     } else {
         return 400
+    }
+}
+
+function cervejaPP (duracao) {
+    let carne = 400;
+    if (duracao >= 6) {
+        return 2000
+    } else {
+        return 1200
     }
 }
