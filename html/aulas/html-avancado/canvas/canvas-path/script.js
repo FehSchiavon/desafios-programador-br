19393b82a2c988e5231ee217f1187de5570936f0
FehSchiavon <felipschiavon@gmail.com>
2021-04-23 06:49:11 -0300

    aprendo a usar o canvas para desenhar

--- html/aulas/html-avancado/canvas/canvas-path/script.js
+++ html/aulas/html-avancado/canvas/canvas-path/script.js
@@ -2,9 +2,36 @@ let tela = document.getElementById('tela')
 
 let context = tela.getContext('2d')
 
-context.moveTo(0, 0)
-context.lineTo(250, 250)
-context.lineTo(500, 0)
-context.strokeStyle = "blue"
-context.lineWidth = 5
+
+// Fazendo uma acao com a linha
+context.beginPath()
+
+context.lineWidth = 4
+context.strokeStyle = "red"
+context.moveTo(10, 10)
+context.lineTo(400, 200)
 context.stroke()
+
+
+// Fazendo outra acao com linha
+context.beginPath()
+
+context.lineWidth = 7
+context.strokeStyle = "blue"
+context.moveTo(30, 10)
+context.lineTo(300, 300)
+context.stroke()
+
+
+// Fazendo mais outra acao com linha
+context.beginPath()
+
+context.lineWidth = 15
+context.strokeStyle = "pink"
+context.fillStyle = "green"
+context.moveTo(250, 70)
+context.lineTo(200, 200)
+context.lineTo(100, 200)
+context.closePath()
+context.stroke()
+context.fill()
