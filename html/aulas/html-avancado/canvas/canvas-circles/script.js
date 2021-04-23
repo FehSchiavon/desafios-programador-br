2db8932b679e109ad6d273423fe1f444f915e728
FehSchiavon <felipschiavon@gmail.com>
2021-04-23 07:01:11 -0300

    aprendo a fazer um pouco de animacao

--- html/aulas/html-avancado/canvas/canvas-circles/script.js
+++ html/aulas/html-avancado/canvas/canvas-circles/script.js
@@ -2,7 +2,6 @@ let tela = document.getElementById('tela')
 
 let context = tela.getContext('2d')
 
-
 let x = 250
 let y = 250
 let raio = 100
@@ -11,8 +10,8 @@ let inicio = 0
 let fim = 2 * Math.PI
 
 context.beginPath()
-context.stokeStyle = "red"
+context.strokeStyle = "red"
 
 context.arc(x, y, raio, inicio, fim)
 
-context.stoke()
+context.stroke()
