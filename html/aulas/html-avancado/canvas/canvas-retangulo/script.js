a89bfa829b346e20e064adfed1eea01effa3476d
FehSchiavon <felipschiavon@gmail.com>
2021-04-22 21:15:05 -0300

    varias formas de criar retangulos

--- html/aulas/html-avancado/canvas/canvas-retangulo/script.js
+++ html/aulas/html-avancado/canvas/canvas-retangulo/script.js
@@ -2,9 +2,21 @@ let tela = document.getElementById('tela')
 
 let context = tela.getContext('2d')
 
-context.moveTo(0, 0)
-context.lineTo(250, 250)
-context.lineTo(500, 0)
-context.strokeStyle = "blue"
+// === Retangulo preenchido === //
+// context.fillStyle = "blue"
+// context.fillRect(10, 10, 100, 200)
+
+// === Retangulo sem preenchimento === //
+// context.strokeStyle = "red"
+// context.strokeRect(10, 10, 100, 200)
+
+// === Outra forma de fazer a aplicação do Retangulo === //
+context.rect(10, 10, 100, 200)
+context.fillStyle = "blue"
 context.lineWidth = 5
+context.strokeStyle = "red"
+
+context.fill()
 context.stroke()
+
+context.clearRect(20, 20, 30, 30)
