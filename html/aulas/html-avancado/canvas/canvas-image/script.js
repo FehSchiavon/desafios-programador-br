6bf176de296dbdeb957623b67d0eaeb27da0e228
FehSchiavon <felipschiavon@gmail.com>
2021-04-25 16:25:28 -0300

    duas formas de colocar uma imagem dentro do canvas

--- html/aulas/html-avancado/canvas/canvas-image/script.js
+++ html/aulas/html-avancado/canvas/canvas-image/script.js
@@ -2,3 +2,18 @@ let tela = document.getElementById('tela')
 
 let context = tela.getContext('2d')
 
+// === Forma 1° de colocar imagem no Canvas === //
+// let img = document.getElementById("bible")
+// context.drawImage(img, 20, 20)
+
+// === Forma 2° de colocar imagem no Canvas === //
+let img = new Image()
+img.src = "./bible.png"
+
+img.onload = desenharImg
+
+function desenharImg() {
+    context.drawImage(this, 20, 20, this.naturalWidth / 3, this.naturalHeight / 3)
+
+}
+
