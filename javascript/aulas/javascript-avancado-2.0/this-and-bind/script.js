7a81b73d93353342c5ad60cf9ab1c774e73f4013
FehSchiavon <felipschiavon@gmail.com>
2021-05-18 06:59:40 -0300

    entendo uma pouco mais como funciona o This no javascript

---
+++ javascript/aulas/javascript-avancado-2.0/this-and-bind/script.js
@@ -0,0 +1,17 @@
+
+function speakGeneric() {
+    console.log(this.sound)
+}
+
+let dog = {
+    sound: "Au Au",
+    speak: speakGeneric
+}
+
+let cat = {
+    sound: "Miau",
+    speak: speakGeneric
+}
+
+dog.speak()
+cat.speak()
