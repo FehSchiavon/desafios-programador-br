310d9e172e312c51e5642778fd59ea45ce894204
FehSchiavon <felipschiavon@gmail.com>
2021-05-26 07:05:51 -0300

    varios niveis de filter ate nivel PRO

--- javascript/aulas/javascript-avancado-2.0/filter/script.js
+++ javascript/aulas/javascript-avancado-2.0/filter/script.js
@@ -16,11 +16,85 @@ for (let aluno of alunos) {
     }
 }
 
+
+
+
+
 // Ou usando o Filter
 
 function temMenosde30(aluno) {
     return aluno.idade < 30
 }
 
-let alunosComMenosde30 = alunos.filter(temMenosde30)
+function temMaisde30(aluno) {
+    return aluno.idade > 30
+}
+
+let alunosComMenosde30 = alunos.filter(temMenosde30) // Trocando temMenosde30 por temMaisde30 ele funciona normalmente
 console.log(alunosComMenosde30)
+
+
+
+
+
+
+// Filter mais elaborado
+function temMenosde30(aluno) {
+    return aluno.idade < 30
+}
+
+function temMaisde30(aluno) {
+    return aluno.idade > 30
+}
+
+function filtro(callback) {
+
+    let alunosFiltrados = []
+    for(let aluno of alunos) {
+        if (callback(aluno)) {
+            alunosFiltrados.push(aluno)
+        }
+    }
+    return alunosFiltrados
+}
+
+console.log(filtro(temMaisde30))
+
+
+
+
+
+// Filter nivel PRO
+let jogadores = [
+    novoAluno("Igor", 23),
+    novoAluno("Marcela", 45),
+    novoAluno("Bruna", 29),
+    novoAluno("Pedro", 35),
+    novoAluno("Josi", 50)
+]
+
+function temMenosde30(aluno) {
+    return aluno.idade < 30
+}
+
+function temMaisde30(aluno) {
+    return aluno.idade > 30
+}
+
+function filtroPRO(callback) {
+
+    let alunosFiltrados = []
+    for(let aluno of this) {
+        if (callback(aluno)) {
+            alunosFiltrados.push(aluno)
+        }
+    }
+    return alunosFiltrados
+}
+
+alunos.filtroPRO = filtroPRO
+jogadores.filtroPRO = filtroPRO
+
+// Desta forma você PODE usar qualquer Array. So trocar o nome do Array
+// console.log(alunos.filtro(temMaisde30))
+console.log(jogadores.filtroPRO(temMaisde30))
