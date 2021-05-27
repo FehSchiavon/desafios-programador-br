0e72937e0e5ba0b3f013ce2d4dca9ba62d96f579
FehSchiavon <felipschiavon@gmail.com>
2021-05-27 06:58:27 -0300

    criando estrutura do map

---
+++ javascript/aulas/javascript-avancado-2.0/map/script.js
@@ -0,0 +1,15 @@
+function novoAluno(nome, idade) {
+    return {nome, idade}
+}
+
+let alunos = [
+    novoAluno("Mario", 23),
+    novoAluno("Luigi", 45),
+    novoAluno("Princesa", 29),
+    novoAluno("Bonzer", 35),
+    novoAluno("Fantasma", 18),
+]
+
+function nomeEidade(aluno) {
+    return aluno
+}
