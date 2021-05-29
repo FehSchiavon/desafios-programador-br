4423bc8cc30971171d19cd554623f1f95287e13f
FehSchiavon <felipschiavon@gmail.com>
2021-05-29 16:42:38 -0300

    o famoso ... e o nosso Spread Operator

--- javascript/aulas/javascript-avancado-2.0/spread-operator/script.js
+++ javascript/aulas/javascript-avancado-2.0/spread-operator/script.js
@@ -8,8 +8,18 @@ var contato = {
     email: "name@gmail.com"
 }
 
-var copia = {...pessoa}
+var copia = {...pessoa, cidade: "Rio de Janeiro", ...contato}
 copia.idade = 88
 
 console.log(pessoa)
 console.log(copia)
+
+// Juntando dos Arrays com Spread Operator
+
+let notasTurma1 = [10, 12, 8, 4, 3]
+
+let notasTurma2 = [4, 5, 6, 8, 9]
+
+let todasNotasdaTurma = [...notasTurma1, ...notasTurma2]
+
+console.log(todasNotasdaTurma)
