c73504ffdcdf6a865537233d53c3d71d1616fb89
FehSchiavon <felipschiavon@gmail.com>
2021-05-31 06:44:40 -0300

    organizando as pastas de html/ javascript

---
+++ javascript/aulas/javascript-avancado/desafio-js-avancado/script.js
@@ -0,0 +1,12 @@
+function criarAlunos(nome, idade) {
+    return {nome, idade}
+}
+
+let alunos = [
+    criarAlunos("Pedro", 25),
+    criarAlunos("Suzana", 23),
+    criarAlunos("Felipe", 28),
+    criarAlunos("Matheus", 19),
+    criarAlunos("Ronaldo", 35)
+]
+
