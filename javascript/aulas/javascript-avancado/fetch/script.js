5638d7e87507e18560ced4ad85124eef19952031
FehSchiavon <felipschiavon@gmail.com>
2021-06-04 20:31:03 -0300

    consulmida no front -end

--- javascript/aulas/javascript-avancado/fetch/script.js
+++ javascript/aulas/javascript-avancado/fetch/script.js
@@ -1,14 +1,26 @@
 let url = "https://economia.awesomeapi.com.br/usd-brl"
 
-fetch(url).
+// fetch(url).
+//     then((res) => {
+//         return res.json()
+//     })
+//     .then((data) => {
+//         console.log(data[0].high)
+//     })
+
+function converter() {
+    let input = document.getElementById("valor")
+    let valor = input.value
+
+    fetch(url).
     then((res) => {
         return res.json()
     })
     .then((data) => {
+        let rate = data[0].high
+        let resultado = `${valor} dolares = ${rate * valor} em reais`
+        document.getElementById("resultado").innerHTML = resultado
+
         console.log(data[0].high)
     })
-
-function converter() {
-    let input = document.getElementById(valor).value
-
 }
